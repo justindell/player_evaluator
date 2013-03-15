01e10ee4f06ff25620c92cef64e0cfe7a50342a2
Justin Dell <dell.justin@gmail.com>
2013-03-15 14:23:42 -0500

    remove old script

--- print_points.rb
+++
@@ -1,9 +0,0 @@
-require './calculate'
-
-results = Calculate.points_per_game
-
-puts "Player                                  Team                                    Points     Games"
-results.each_with_index do |result, idx|
-  puts "%-40s%-40s%0.4f    %s\n" % ["#{idx + 1}. #{result[:player]}", "#{result[:team_rpi]} #{result[:team]}", result[:points], result[:games]]
-end
-
