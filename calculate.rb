78f8840c80a1d92740853a31bfb9503db7f7a4d4
Justin Dell <dell.justin@gmail.com>
2013-03-15 18:31:23 -0500

    refactor

--- calculate.rb
+++ calculate.rb
@@ -9,30 +9,25 @@ class Calculate
       format = "%-40s%-40s%0.4f    %s\n"
       opponent_rpi = opts['opponent_rpi'].empty? ? 100 : opts['opponent_rpi']
       min_games = opts['min_games'].empty? ? 0 : opts['min_games']
+      seed = opts['seed'].empty? ? 16 : opts['seed']
+      seed_join = opts['team_id'].empty? ? lambda{|t,b,js| :seed.qualify(t) <= seed} : lambda{|t,b,js| {:id.qualify(t) => opts['team_id']}}
+      team_join = opts['opponent_rpi'].empty? ? lambda{|o,b,js| {1 => 1}} : lambda{|o,b,js| :rpi.qualify(o) <= opponent_rpi}
+      draft_filter = opts['hide_drafted'].nil? ? {true => true}  : {:players__drafted => false}
 
-      query = DB[:boxscores].select(:players__id.as(:id),
-                                    :players__name.as(:player),
-                                    :players__drafted.as(:drafted),
-                                    :teams__name.as(:team),
-                                    :teams__rpi.as(:team_rpi),
-                                    :teams__seed.as(:seed)).
-                            select_more{[avg(:boxscores__points).as(:points), count(:boxscores__points).as(:games)]}.
-                            group(:players__name, :players__drafted, :teams__name).
-                            having{count(:boxscores__points) >= min_games.to_i}.
-                            order{avg(:boxscores__points).desc}
-      query = opts['opponent_rpi'].empty? ?
-        query.join(:teams, {:id => :opponent_id}, {:table_alias => :opponents}) :
-        query.join(:teams, {:id => :opponent_id}, {:table_alias => :opponents}) {|o,b,js| :rpi.qualify(o) <= opponent_rpi}
-      query = if opts['team_id'].empty?
-                opts['seed'].empty? ?
-                  query.join(:teams, :id => :players__team_id) {|t,b,js| :seed.qualify(t) <= 16} :
-                  query.join(:teams, :id => :players__team_id) {|t,b,js| :seed.qualify(t) <= opts['seed']}
-              else
-                query.join(:teams, :id => :players__team_id){|t,b,js| {:id.qualify(t) => opts['team_id']}}
-              end
-      query = query.join(:players, :id => :boxscores__player_id)
-      query = query.filter(~{:players__drafted => true}) if opts['hide_drafted']
-      query
+      DB[:boxscores].select(:players__id.as(:id),
+                            :players__name.as(:player),
+                            :players__drafted.as(:drafted),
+                            :teams__name.as(:team),
+                            :teams__rpi.as(:team_rpi),
+                            :teams__seed.as(:seed)).
+                    select_more{[avg(:boxscores__points).as(:points), count(:boxscores__points).as(:games)]}.
+                    group(:players__name, :players__drafted, :teams__name).
+                    having{count(:boxscores__points) >= min_games.to_i}.
+                    order{avg(:boxscores__points).desc}.
+                    join(:teams, {:id => :opponent_id}, {:table_alias => :opponents}) {|o,b,js| team_join.call(o,b,js)}.
+                    join(:teams, :id => :players__team_id) {|t,b,js| seed_join.call(t,b,js)}.
+                    join(:players, :id => :boxscores__player_id).
+                    where(draft_filter)
     end
 
     def expected_wins seed
